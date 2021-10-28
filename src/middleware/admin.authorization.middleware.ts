7b3c16a170933c152d2fcbfddf81199fe2a28bb4
tuantrung <tuantrung.dev@gmail.com>
2021-10-29 00:33:58 +0700

    update admin auth

--- src/middleware/admin.authorization.middleware.ts
+++ src/middleware/admin.authorization.middleware.ts
@@ -14,9 +14,10 @@ export const checkAdminAuthorized = async (
   }
 
   try {
-    const token = req.headers['authorization'].split(' ')[1]
-
-    const payload = jwt.verify(token || '', config.jwtPrivateKey)
+    const payload = jwt.verify(
+      req.headers['authorization'] || '',
+      config.jwtPrivateKey
+    )
 
     if (typeof payload === 'object') {
       const existingAdmin = AdminModel.findById(payload._id)
