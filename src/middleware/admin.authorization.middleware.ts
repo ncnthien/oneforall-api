e58b37a248d9a8285979ab389e68cc477df5d853
ncnthien <nhatthien185@gmail.com>
2021-10-28 22:30:08 +0700

    update auth api

--- src/middleware/admin.authorization.middleware.ts
+++ src/middleware/admin.authorization.middleware.ts
@@ -14,7 +14,7 @@ export const checkAdminAuthorized = async (
   }
 
   try {
-    const token = req.headers['authorization'].split(' ')[1]
+    const token = req.headers['authorization']
 
     const payload = jwt.verify(token || '', config.jwtPrivateKey)
 
